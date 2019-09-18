5d81ab23349acf028079f7e930972aa59dac114b
Shane Gallatin <shane.gallatin@verizon.net>
2019-09-18 16:18:02 -0500

    finished four problems

--- practice.js
+++ practice.js
@@ -104,7 +104,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
